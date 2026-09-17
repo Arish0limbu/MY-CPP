8d0215f9754a5bd441718315fbcf9f2dcd27ebc1
Arish limbu <limbuarish0@gmail.com>
2026-09-17 22:46:44 +0545

    class and object

--- try.cpp
+++ try.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 class interest
 {
 private:
-    float p, r, t, si;
+    float p, t, r, si;
 
 public:
     void input()
     {
-        
+        cout << "Enter p t r: ";
+        cin >> p >> t >> r;
+    }
+
+    void calculate()
+    {
+        si = (p * t * r)/100;
+    }
+
+    void display()
+    {
+        cout << "Simple interest is= " << si;
     }
 };
+
+interest simple(interest obj)
+{
+    obj.calculate();
+    return obj;
+}
+
+int main()
+{
+    interest a,b;
+    a.input();
+    b = simple(a);
+    b.display();
+    return 0;
+}
